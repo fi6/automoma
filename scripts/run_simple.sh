6e252e4de17ee599f11866c03de94ee9f78e726b
walkerch <changxinhai@stu.pku.edu.cn>
2025-10-28 18:37:06 +0800

    chore: update run script

--- scripts/run_simple.sh
+++ scripts/run_simple.sh
@@ -1,2 +1,6 @@
 python scripts/pipeline_collect.py --scene_dir output/collect/infinigen_scene_100 \
      --plan_dir output/collect/traj --robot_name summit_franka --num_episodes 1000
+
+
+python scripts/pick_data_automoma.py --mode collect
+python /home/xinhai/automoma/scripts/pick_data_automoma.py --mode pick --link
